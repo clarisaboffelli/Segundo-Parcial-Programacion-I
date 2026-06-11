4be3fda35adf750095ebe3e31bedc4d49bae3200
clarisaboffelli@gmail.com <@clarisaboffelli>
2026-06-11 20:47:25 -0300

    Estructuracion del menu principal
    Se corrigieron las funciones de menu, se agrego la validacion en cada una. Ademas se estructuro el menu principal con las opciones match/case y se agregaron docstrings

--- Ferreteria_inventario.py
+++ Ferreteria_inventario.py
@@ -3,18 +3,46 @@
 # ============================================================= #
 
 # Se define una funcion que presenta el menu principal.
+
 def menu_principal():
+    """
+    Muestra por pantalla el menú principal y solicita al usuario la entrada de menú a la que desea acceder.
+
+    Parametro:
+    opcion (str): Selección de la entrada del menú por parte del usuario.
+
+    Retorna:
+        str: La entrada de menú seleccionada por el usuario.
+    """
+
     print("""\n MENU PRINCIPAL
-    1. Carga inicial de herramientas.
+    1. Carga de Herramientas con Existencias Iniciales.
     2. Visualización del inventario.
     3. Consulta de stock.
     4. Reporte de agotados.
     5. Alta de nuevo producto.
     6. Actualización de stock.
     7. Salir.""")
+    
+    opcion_mp = validar_opcion(1,7)
 
-# Se define una funcion que muestra el menu secundario de la opcion 6. Actualizacion de stock.
+    return opcion_mp
+
+
+# Se define una funcion que muestra el menu secundario de la opcion 6. Actualizacion de stock, y solicita al usuario la opcion de menú seleccionada.
+
 def menu_secundario():
+
+    """
+    Muestra por pantalla el menú secundario correspondiente a la opción 6 del menú principal y solicita al usuario la entrada de menú a la que desea acceder.
+
+    Parametro:
+    opcion (str): Selección de la entrada del menú por parte del usuario.
+
+    Retorna:
+        str: La entrada de menú seleccionada por el usuario.
+    """
+
     print("""\n MENU ACTUALIZACION DE STOCK.
     1. Venta.
     2. Ingreso.
@@ -32,7 +60,7 @@ def validar_opcion(minimo_vo,maximo_vo):
             if minimo_vo <= opcion_vo <= maximo_vo:
                 return opcion_vo
             else:
-                print(f"Solo se permite el ingreso de los números {minimo_vo} a {maximo_vo}")
+                print(f"Solo se permite el ingreso de los números {minimo_vo} a {maximo_vo}.")
         except ValueError:
             print("Ingrese el valor númerico correspondiente a la acción detallada en el menú.")
         except Exception as e:
@@ -44,7 +72,24 @@ def validar_opcion(minimo_vo,maximo_vo):
 # ============================================================= #
 # PROGRAMA PRINCIPAL
 # ============================================================= #
+run_menu = True
+while run_menu:
+    opcion = menu_principal()
+    match opcion:
+        case "1":
+            print("\nCARGA DE HERRAMIENTAS CON EXISTENCIAS INICIALES")
+        case "2": 
+            print("\nVISUALIZACION DE INVENTARIO")
+        case "3":
+            print("\nCONSULTA DE STOCK")
+        case "4":
+            print("\nREPORTE DE AGOTADOS")
+        case "5":
+            print("\nALTA DE NUEVO PRODUCTO")
+        case "6":
+            print("\nACTUALIZACION DE STOCK (VENTA / INGRESO)")
+        case "7":
+            print("\nSALIR")
+            run_menu = False
 
-menu_principal()
-opcion = validar_opcion(1,7)
 
